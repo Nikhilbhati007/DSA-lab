ef01d1f6777b24417d9b3c4f83a0c02a61e6c68a
Nikhilbhati007 <nikhilbhati1827ai@gmail.com>
2025-01-23 09:27:22 +0530

    commit

--- insert.c
+++ insert.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 int main(){
-    int a[50],size,i,pos,num;
+    int a[50],size,pos,num,i;
     printf("Enter the size of the array: ");
     scanf("%d",&size);
-    printf("Enter the elements of the array: ");
-    for(i=0;i<size;i++){
-        scanf("%d",&a[i]);
+    if(size>50){
+        printf("Array size exceeded");
+        return 0;
     }
-    printf("Enter the position where you want to insert the element: ");
-    scanf("%d",&pos);
-    printf("Enter the element you want to insert: ");
-    scanf("%d",&num);
-    for(i=size-1;i>=pos-1;i--){
-        a[i+1]=a[i];
+    else{
+        printf("Enter the position you want to insert the element: ");
+        scanf("%d",&pos);
+        for(i=size;i>=pos;i--){
+            a[i]=a[i-1];
+        }
+        printf("Enter the number you want to insert: ");
+        scanf("%d",&num);
+        a[pos-1]=num;
     }
-    a[pos-1]=num;
-    size++;
-    printf("The array after insertion is: ");
-    for(i=0;i<size;i++){
-        printf("%d ",a[i]);
-    }
+    for 
     return 0;
 }
